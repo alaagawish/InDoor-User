774b649928c206512ab3c458939f18b8a4cafaf5
Alaa Gawish <53160167+alaagawish@users.noreply.github.com>
2023-06-03 23:37:16 +0300

    home UI

---
+++ InDoor-User/Module/Home/View/HomeViewController.swift
@@ -0,0 +1,100 @@
+//
+//  HomeViewController.swift
+//  InDoor-User
+//
+//  Created by Alaa on 03/06/2023.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    @IBOutlet weak var brandCollectionView: UICollectionView!
+    @IBOutlet weak var sliderCollectionView: UICollectionView!
+    let discountArray = ["discount1",
+                         "discount2",
+                         "discount5",
+                         "discount3"]
+    var timer: Timer?
+    var currentIndex = 0
+    var brands:[String] = ["NIKE","ADDIDAS","POMA"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        pageControl.numberOfPages = discountArray.count
+        
+        startTimer()
+    }
+    func startTimer(){
+        timer = Timer.scheduledTimer(timeInterval: 1.5, target: self, selector: #selector(moveSlider), userInfo: nil, repeats: true)
+    }
+    @objc
+    func moveSlider(){
+        currentIndex = (currentIndex + 1) % sliderCollectionView.numberOfItems(inSection: 0)
+        self.sliderCollectionView.scrollToItem(at: IndexPath(item: currentIndex, section: 0), at: .centeredHorizontally, animated: true)
+        pageControl.currentPage = currentIndex
+        
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if collectionView == sliderCollectionView{
+            return discountArray.count
+        }else{
+            return brands.count
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if collectionView == sliderCollectionView{
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "slider", for: indexPath) as! SliderCollectionViewCell
+            
+            cell.discountImage.image = UIImage(named: discountArray[indexPath.row])
+            return cell
+            
+        }else{
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "brandCell", for: indexPath) as! BrandCollectionViewCell
+            
+            cell.setValues(brandName: brands[indexPath.row], brandImage: "")
+            
+            return cell
+            
+        }
+        
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        if collectionView == sliderCollectionView{
+            return CGSize(width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.height/3)
+        }else{
+            return CGSize(width: UIScreen.main.bounds.size.width/2 - 10, height: UIScreen.main.bounds.height/4 - 10)
+        }
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        if collectionView == sliderCollectionView{
+            return 0
+        }else{
+            return 10
+        }
+    }
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if collectionView == sliderCollectionView{
+            print(indexPath.row)
+            print(discountArray[indexPath.row])
+        }else{
+            
+        }
+    }
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "brandHeader", for: indexPath) as! HeaderCollectionReusableView
+        header.headerName.text = "Brands"
+        return header
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        timer?.invalidate()
+    }
+    
+}
