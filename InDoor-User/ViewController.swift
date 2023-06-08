37d713921f595adc7b2975e3d60cf10e976462e9
Alaa Gawish <53160167+alaagawish@users.noreply.github.com>
2023-06-09 01:29:43 +0300

    editing in main

--- InDoor-User/ViewController.swift
+++ InDoor-User/ViewController.swift
@@ -18,16 +18,17 @@ class ViewController: UIViewController {
         let storyboard = UIStoryboard(name: "Home", bundle: nil)
         let recipeDetails = storyboard.instantiateViewController(withIdentifier: "tabbar") as! MainTabBarController
         recipeDetails.modalPresentationStyle = .fullScreen
-      
-    
-
+        
+        
+        
         present(recipeDetails, animated: true)
-    @IBAction func navigateToFav(_ sender: UIButton) {
-        let storyboard = UIStoryboard(name: "FavoritesStoryBoard", bundle: nil)
-                let FavoritesStoryBoard = storyboard.instantiateViewController(withIdentifier: "FavoritesStoryBoard") as! FavoritesViewController
-        FavoritesStoryBoard.modalPresentationStyle = .fullScreen
-                present(FavoritesStoryBoard, animated: true)
     }
+//    @IBAction func navigateToFav(_ sender: UIButton) {
+//        let storyboard = UIStoryboard(name: "FavoritesStoryBoard", bundle: nil)
+//                let FavoritesStoryBoard = storyboard.instantiateViewController(withIdentifier: "FavoritesStoryBoard") as! FavoritesViewController
+//        FavoritesStoryBoard.modalPresentationStyle = .fullScreen
+//                present(FavoritesStoryBoard, animated: true)
+//    }
     
 }
 
