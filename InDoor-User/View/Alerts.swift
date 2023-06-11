29a5f7c197e6c9acaa0a9b84ff08ea56c0deff7a
Safiya Fikry <safiyafikry@gmail.com>
2023-06-12 01:56:30 +0300

    add two functions for alert

--- InDoor-User/View/Alerts.swift
+++ InDoor-User/View/Alerts.swift
@@ -22,5 +22,18 @@ class Alert{
         alert.addAction(UIAlertAction(title: Constants.yes, style: .default, handler: yesHandler))
         return alert
     }
+    
+    func showAlertWithNegativeAndPositiveButtons(title: String, msg: String,negativeButtonTitle: String, positiveButtonTitle: String, negativeHandler: ((UIAlertAction)->())? = nil, positiveHandler:@escaping (UIAlertAction)->())-> UIAlertController{
+        let alert : UIAlertController = UIAlertController(title: title, message:msg, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: negativeButtonTitle, style: .cancel, handler: negativeHandler))
+        alert.addAction(UIAlertAction(title: positiveButtonTitle, style: .default, handler: positiveHandler))
+        return alert
+    }
+    
+    func showAlertWithPositiveButtons(title: String, msg: String, positiveButtonTitle: String, positiveHandler:((UIAlertAction)->())? = nil)-> UIAlertController{
+        let alert : UIAlertController = UIAlertController(title: title, message:msg, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: positiveButtonTitle, style: .default, handler: positiveHandler))
+        return alert
+    }
 }
 
