814277e3d12601e330addb48563f5b9d090196ff
Safiya Fikry <safiyafikry@gmail.com>
2023-06-12 01:56:01 +0300

    add constants

--- InDoor-User/Utils/Constants.swift
+++ InDoor-User/Utils/Constants.swift
@@ -45,6 +45,25 @@ class Constants {
     static let homeIdentifier = "tabbar"
     static let signUpIdentifier = "signup"
     static let loginIdentifier = "login"
+    static let no = "NO"
+    static let warning = "Warning !!"
+    static let invalidEmail = "Invalid Email."
+    static let emailUsedBefore = "Sorry, You used this email before in registeration.\nDo you want to login?"
+    static let enterAllData = "Please, Enter your data to register."
+    static let firstNameIsEmpty = "Please, Enter your first name."
+    static let lastNameIsEmpty = "Please, Enter your last name."
+    static let emailIsEmpty = "Please, Enter your email."
+    static let passwordIsEmpty = "Please, Enter your password."
+    static let confirmPasswordIsEmpty = "Please, Enter password confirmation."
+    static let cityIsEmpty = "Please, Enter your city."
+    static let addressIsEmpty = "Please, Enter your address."
+    static let postalCodeIsEmpty = "Please, Enter the postal code/zip."
+    static let phoneIsEmpty = "Please, Enter your phone number."
+    static let passwordAndConfirmPasswordShouldBeTheSame = "Sorry, password and confirm password should be the same."
+    static let registeredSuccessfully = "Congrats, you registered successfully."
+    static let checkEmailAndPassword = "Invalid data\nPlease, check your email and password."
+    static let emailAndPasswordCannotBeEmpty = "Please, enter your email and your password."
+    
     
     
 }
