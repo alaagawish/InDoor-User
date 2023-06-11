94f70f4f26415d65eace547ec9a4b86fa6e61be5
Safiya Fikry <safiyafikry@gmail.com>
2023-06-12 02:00:35 +0300

    add loginViewModel class
    get user data and check if registered or not

--- InDoor-User/Module/Login/View/LoginViewController.swift
+++ InDoor-User/Module/Login/View/LoginViewController.swift
@@ -9,12 +9,71 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    var loginViewModel: LoginViewModel!
+    var found = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
+        loginViewModel = LoginViewModel(service: Network())
+        loginViewModel.bindToUsersListSignUpController = { [weak self] in
+            if let list = self?.loginViewModel.usersList{
+                for user in list {
+                    if user.email == self?.emailTextField.text && user.tags == self?.passwordTextField.text {
+                        self?.found = true
+                        break
+                    }
+                }
+            }
+            
+            if self?.found == true {
+                DispatchQueue.main.async {
+                        let storyboard = UIStoryboard(name: Constants.homeStoryboardName, bundle: nil)
+                        let home = storyboard.instantiateViewController(withIdentifier: Constants.homeIdentifier) as! MainTabBarController
+                        home.modalPresentationStyle = .fullScreen
+                        self?.present(home, animated: true)
+                }
+            } else {
+                DispatchQueue.main.async {
+                    let alert = Alert().showAlertWithPositiveButtons(title: Constants.warning, msg: Constants.checkEmailAndPassword, positiveButtonTitle: Constants.ok, positiveHandler: nil)
+                    self?.present(alert, animated: true)
+                }
+            }
+        }
     }
     
     @IBAction func navigateBack(_ sender: UIButton) {
         self.dismiss(animated: true)
     }
 
+    @IBAction func login(_ sender: UIButton) {
+        if(emailTextField.text == "" && passwordTextField.text == ""){
+            let alert = Alert().showAlertWithPositiveButtons(title: Constants.warning, msg: Constants.emailAndPasswordCannotBeEmpty, positiveButtonTitle: Constants.ok, positiveHandler: nil)
+            self.present(alert, animated: true)
+        }
+        else if (emailTextField.text == ""){
+            let alert = Alert().showAlertWithPositiveButtons(title: Constants.warning, msg: Constants.emailIsEmpty, positiveButtonTitle: Constants.ok, positiveHandler: nil)
+            self.present(alert, animated: true)
+        }
+        else if (passwordTextField.text == ""){
+            let alert = Alert().showAlertWithPositiveButtons(title: Constants.warning, msg: Constants.passwordIsEmpty, positiveButtonTitle: Constants.ok, positiveHandler: nil)
+            self.present(alert, animated: true)
+        }
+        else{
+            loginViewModel.getUser()
+        }
+    }
+    func setupUI(){
+        setupUIView(uiView: emailTextField)
+        setupUIView(uiView: passwordTextField)
+    }
+    
+    func setupUIView(uiView: UIView){
+        uiView.layer.cornerRadius = 12
+        uiView.layer.borderWidth = 1.0
+        uiView.layer.borderColor = UIColor.black.cgColor
+        uiView.layer.masksToBounds = true
+    }
 }
