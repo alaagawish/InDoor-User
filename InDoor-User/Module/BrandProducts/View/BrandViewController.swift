4a9f95cdde85a7c60f109235b2c0ef49f5d58d49
Alaa Gawish <53160167+alaagawish@users.noreply.github.com>
2023-06-11 05:16:29 +0300

    search in brand products

--- InDoor-User/Module/BrandProducts/View/BrandViewController.swift
+++ InDoor-User/Module/BrandProducts/View/BrandViewController.swift
@@ -6,28 +6,56 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class BrandViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
+    
+    @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var productsCollectionView: UICollectionView!
     var brandViewModel: BrandViewModel!
     var products: [Product] = []
+    var orginList: [Product] = []
     var id: Int!
     var favoritesViewModel: FavoritesViewModel!
+    var searchQuery: BehaviorRelay<Product>!
+    var disposeBag: DisposeBag!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         productsCollectionView.register(UINib(nibName: Constants.brandProductCollectionViewCell, bundle: nil), forCellWithReuseIdentifier: Constants.brandProduct)
-        
+        disposeBag = DisposeBag()
         brandViewModel = BrandViewModel(netWorkingDataSource: Network())
         callingData()
         favoritesViewModel = FavoritesViewModel(service: DatabaseManager.instance)
+        search()
     }
-    func callingData(){
+    func search() {
+        searchBar.rx.text.subscribe{[weak self] text in
+            guard let self = self else {return}
+            self.filter(searchText: text!)
+        }.disposed(by: disposeBag)
+        
+    }
+    func filter(searchText:String) {
+        if(!searchText.isEmpty){
+            products = orginList.filter{(Splitter().splitName(text: $0.title!, delimiter: "| ").lowercased().contains(searchText.lowercased()))}
+            if products.isEmpty{
+                products = []
+            }
+        }else {
+            products = orginList
+        }
+        self.productsCollectionView.reloadData()
+    }
+    func callingData() {
         brandViewModel.bindResultToViewController = {[weak self] in
             DispatchQueue.main.async {
                 self?.products = self?.brandViewModel.result ?? []
+                self?.orginList = self?.brandViewModel.result ?? []
                 self?.productsCollectionView.reloadData()
-                
+                self?.searchQuery = BehaviorRelay<Product>(value: (self?.products[0])!)
             }
         }
         brandViewModel.getItems(id: id)
@@ -40,12 +68,12 @@ class BrandViewController: UIViewController, UICollectionViewDelegate, UICollect
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Constants.brandProduct, for: indexPath) as! BrandProductCollectionViewCell
         cell.setValues(product: products[indexPath.row], isFav: favoritesViewModel.checkIfProductIsFavorite(productId: products[indexPath.row].id), viewController: self)
-
+        
         return cell
     }
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: UIScreen.main.bounds.width/2 - 10, height: UIScreen.main.bounds.height/4 - 12)
-
+        
     }
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return 10
@@ -61,5 +89,4 @@ class BrandViewController: UIViewController, UICollectionViewDelegate, UICollect
     @IBAction func back(_ sender: Any) {
         self.dismiss(animated: true)
     }
-    
 }
