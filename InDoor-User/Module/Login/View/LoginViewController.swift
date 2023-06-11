f39ba8dd3eae6b9c015001547797f47e0061e8de
Safiya Fikry <safiyafikry@gmail.com>
2023-06-11 03:32:18 +0300

    add viewControllers for welcome, signUp, login screens
    for welcome screen add navigation to home, login ,signUp screens
    for login and signUp screens add navigation back

---
+++ InDoor-User/Module/Login/View/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  InDoor-User
+//
+//  Created by Mac on 11/06/2023.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func navigateBack(_ sender: UIButton) {
+        self.dismiss(animated: true)
+    }
+
+}
---
+++ InDoor-User/Module/SignUp/View/SignUpViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SignUpViewController.swift
+//  InDoor-User
+//
+//  Created by Mac on 11/06/2023.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func navigateBack(_ sender: UIButton) {
+        self.dismiss(animated: true)
+    }
+}
