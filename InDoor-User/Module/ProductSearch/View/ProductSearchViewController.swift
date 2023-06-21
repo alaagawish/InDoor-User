da0ed0d15e8007a49ec51615de90cfee955e9fbc
Alaa Gawish <53160167+alaagawish@users.noreply.github.com>
2023-06-21 17:11:16 +0300

    filter in search page

--- InDoor-User/Module/ProductSearch/View/ProductSearchViewController.swift
+++ InDoor-User/Module/ProductSearch/View/ProductSearchViewController.swift
@@ -10,13 +10,18 @@ import RxSwift
 import RxCocoa
 
 class ProductSearchViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-   
- 
+    
+    
+    @IBOutlet weak var bestSellerFloatingButton: UIButton!
+    @IBOutlet weak var priceFloatingButton: UIButton!
     @IBOutlet weak var inputSearchBar: UISearchBar!
     
-
+    @IBOutlet weak var alphabetFloatingButton: UIButton!
+    @IBOutlet weak var sortingButton: UIButton!
+    
     @IBOutlet weak var searchTableView: UITableView!
     var disposeBag: DisposeBag!
+    var sortingTypesFlag = false
     var productSearchViewModel: ProductSearchViewModel!
     var products: [Product] = []
     var productList: [Product] = []
@@ -36,6 +41,29 @@ class ProductSearchViewController: UIViewController, UITableViewDelegate, UITabl
             productList = products
             search()
         }
+        setUpUI()
+    }
+    func setUpUI(){
+        if !sortingTypesFlag{
+            alphabetFloatingButton.isHidden = true
+            priceFloatingButton.isHidden = true
+            bestSellerFloatingButton.isHidden = true
+        }
+        sortingButton.layer.shadowRadius = 10
+        sortingButton.layer.shadowOpacity = 0.3
+        sortingButton.layer.cornerRadius = 30
+        
+        alphabetFloatingButton.layer.shadowRadius = 10
+        alphabetFloatingButton.layer.shadowOpacity = 0.3
+        alphabetFloatingButton.layer.cornerRadius = 30
+        
+        bestSellerFloatingButton.layer.shadowRadius = 10
+        bestSellerFloatingButton.layer.shadowOpacity = 0.3
+        bestSellerFloatingButton.layer.cornerRadius = 30
+        
+        priceFloatingButton.layer.shadowRadius = 10
+        priceFloatingButton.layer.shadowOpacity = 0.3
+        priceFloatingButton.layer.cornerRadius = 30
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -47,7 +75,7 @@ class ProductSearchViewController: UIViewController, UITableViewDelegate, UITabl
         cell.setProductToTableCell(product: products[indexPath.row])
         return cell
     }
-
+    
     @IBAction func navigateBack(_ sender: UIButton) {
         self.dismiss(animated: true)
     }
@@ -88,4 +116,45 @@ class ProductSearchViewController: UIViewController, UITableViewDelegate, UITabl
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         Constants.favoritesCellHeight
     }
+    
+    @IBAction func filter(_ sender: Any) {
+        sortingTypesFlag = !sortingTypesFlag
+        if sortingTypesFlag {
+            alphabetFloatingButton.isHidden = false
+            priceFloatingButton.isHidden = false
+            bestSellerFloatingButton.isHidden = false
+        }else {
+            alphabetFloatingButton.isHidden = true
+            priceFloatingButton.isHidden = true
+            bestSellerFloatingButton.isHidden = true
+        }
+    }
+    
+    @IBAction func filterByRatting(_ sender: Any) {
+        sortingTypesFlag = false
+        alphabetFloatingButton.isHidden = true
+        priceFloatingButton.isHidden = true
+        bestSellerFloatingButton.isHidden = true
+        products = products.sorted(by:  {Float($0.templateSuffix ?? "") ?? 0 > Float($1.templateSuffix ?? "") ?? 0})
+        searchTableView.reloadData()
+    }
+    @IBAction func filterByPrice(_ sender: Any) {
+        sortingTypesFlag = false
+        alphabetFloatingButton.isHidden = true
+        priceFloatingButton.isHidden = true
+        bestSellerFloatingButton.isHidden = true
+        products = products.sorted(by:  {Float($0.variants?[0].price ?? "") ?? 0 < Float($1.variants?[0].price ?? "") ?? 0})
+        searchTableView.reloadData()
+    }
+    
+    
+    @IBAction func filterAlphabetically(_ sender: Any) {
+        sortingTypesFlag = false
+        alphabetFloatingButton.isHidden = true
+        priceFloatingButton.isHidden = true
+        bestSellerFloatingButton.isHidden = true
+        products = products.sorted(by:  {Splitter().splitName(text: $0.title ?? "", delimiter: "| ") < Splitter().splitName(text: $1.title ?? "", delimiter: "| ") })
+        searchTableView.reloadData()
+    }
+    
 }
