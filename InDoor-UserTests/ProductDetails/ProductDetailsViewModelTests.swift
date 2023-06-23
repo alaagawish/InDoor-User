8ec3732ca1e1a789bfe738ec43fe6d96c64935fd
Alaa Gawish <53160167+alaagawish@users.noreply.github.com>
2023-06-23 15:10:58 +0300

    functions in product details

--- InDoor-UserTests/ProductDetails/ProductDetailsViewModelTests.swift
+++ InDoor-UserTests/ProductDetails/ProductDetailsViewModelTests.swift
@@ -8,16 +8,35 @@
 import XCTest
 @testable import InDoor_User
 final class ProductDetailsViewModelTests: XCTestCase {
-
+    
     var network: NetworkProtocol!
-    var productDetailsViewModel: ProductDetailsViewModel!
+    //    var productDetailsViewModel: ProductDetailsViewModel!
     override func setUpWithError() throws {
         network = NetworkMock(isSuccess: true)
-        productDetailsViewModel = ProductDetailsViewModel(service: network as! DatabaseService)
+        //        productDetailsViewModel = ProductDetailsViewModel(service: network as! DatabaseService)
     }
-
+    
     override func tearDownWithError() throws {
     }
-
+    
+    func testAddProduct() {
+        //        let product: LocalProduct
+        //        service.insertProduct(product: product)
+    }
+    
+    func testRemoveProduct() {
+        //        let product: LocalProduct
+        //        service.deleteProduct(product: product)
+    }
+    
+    func testCheckIfProductIsFavorite() {
+        //        let productId: Int, customerId: Int
+        //        return service.isFavorite(productId: productId,customerId: customerId)
+    }
+    
+    func testGetProduct() {
+        //        let productId: Int
+        //        return service.fetchProduct(productId: productId)
+    }
     
 }
